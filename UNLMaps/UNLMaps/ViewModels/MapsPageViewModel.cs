bfa8103385936fa7bf15768cda7ed6ef726827df
Artem <Artem@ACER>
2017-03-05 20:47:37 +0300

    Work with map pins. Added indicator

--- UNLMaps/UNLMaps/ViewModels/MapsPageViewModel.cs
+++ UNLMaps/UNLMaps/ViewModels/MapsPageViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Prism.Commands;
+using Prism.Events;
 using Prism.Navigation;
 using Realms;
 using UNLMaps.Models;
@@ -13,8 +14,10 @@ namespace UNLMaps.ViewModels
     public class MapsPageViewModel : ViewModelBase
     {
         private readonly INavigationService _navigationService;
-        
-        private readonly Realm _realm;
+      
+        public readonly Realm _realm;
+
+        public MapPin AddedMapPin;
 
         // Even though this is IEnumerable<User>, under the hood
         // it also implements INotifyCollectionChanged
@@ -27,28 +30,45 @@ namespace UNLMaps.ViewModels
 
         #endregion
 
-        public MapsPageViewModel(INavigationService navigationService)
+        public MapsPageViewModel(INavigationService navigationService, IEventAggregator eventAggregator)
         {
             _navigationService = navigationService;
-
+           
             _realm = Realm.GetInstance("UNLRealm");
 
-            MapPins = _realm.All<MapPin>();
+            MapPins = GetAllMapPins();
 
             // Delete an object with a transaction
 //            using (var trans = _realm.BeginWrite())
 //            {
-//                foreach (var mP in MapPins)
+//                foreach (var mP in MapPins.ToList())
 //                {
 //                    _realm.Remove(mP);
 //                 
 //                }
 //                trans.Commit();
 //            }
-//
-//            MapPins = _realm.All<MapPin>();
-            var count = MapPins.Count();
             NavigateToCommand = new DelegateCommand(NavigateTo);
+        }
+
+        /// <summary>
+        /// Get all MapPins from DB
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<MapPin> GetAllMapPins()
+        {
+            return _realm.All<MapPin>();
+        }
+
+        /// <summary>
+        /// Get map pin by Id
+        /// </summary>
+        /// <param name="id"> MapPin Id</param>
+        /// <returns></returns>
+        public MapPin GetMapPinById(string id)
+        {
+            var findMapPin = _realm.All<MapPin>().FirstOrDefault(m => m.Id == id);
+            return findMapPin;
         }
 
         /// <summary>
@@ -68,11 +88,10 @@ namespace UNLMaps.ViewModels
         {
             base.OnNavigatedTo(parameters);
 
-            if (parameters.Count != 0)
-            {
-                var b = parameters;
-            }
-          
+            if (parameters.Count == 0) return;
+
+            var newMapPin = parameters["AddedMapPin"] as MapPin;
+            AddedMapPin = newMapPin;
         }
 
     }
--- UNLMaps/UNLMaps/Views/MapsPage.xaml.cs
+++ UNLMaps/UNLMaps/Views/MapsPage.xaml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Android.Gms.Maps;
+using Realms;
+using UNLMaps.ViewModels;
 using UNLMaps.Views.CustomLayouts;
 using Xamarin.Forms;
 using Xamarin.Forms.GoogleMaps;
@@ -12,42 +14,131 @@ namespace UNLMaps.Views
 {
     public partial class MapsPage : ContentPage
     {
+        private MapsPageViewModel mpVM;
+
+        private Geocoder geocoder;
+
+        private bool isStart;
+
         public MapsPage()
         {
             InitializeComponent();
-           // map.HasZoomEnabled = true;
+            mpVM = this.BindingContext as MapsPageViewModel;
+            InitializeMap();
+            isStart = true;
         }
 
-        protected override async void OnAppearing()
+        protected override void OnAppearing()
         {
             base.OnAppearing();
-            Position position = new Position();
-            var geocoder = new Xamarin.Forms.GoogleMaps.Geocoder();
-            var positions = await geocoder.GetPositionsForAddressAsync("Minsk");
-            if (positions.Count() > 0)
+            
+            if(!mpVM.MapPins.Any() || isStart) return;
+
+            var addedPin = mpVM.MapPins.LastOrDefault();
+
+            AddPinToMap(addedPin.Address, addedPin.Name, addedPin.Description, $"{addedPin.Raiting} ★");
+    
+        }
+
+        /// <summary>
+        /// Method populate Pin to Map
+        /// </summary>
+        /// <param name="address"></param>
+        /// <param name="name"></param>
+        /// <param name="description"></param>
+        /// <param name="raiting"></param>
+        private async void AddPinToMap(string address, string name, string  description, string raiting)
+        {
+            Position pinPosition = new Position();
+
+            var pinPositions = await geocoder.GetPositionsForAddressAsync(address);
+
+            if (pinPositions != null && pinPositions.Any())
             {
-                var pos = positions.First();
-                map.MoveToRegion(MapSpan.FromCenterAndRadius(pos, Distance.FromKilometers(2)));
-                var reg = map.VisibleRegion;
-                var format = "0.00";
-                position = pos;
+                pinPosition = pinPositions.First();
             }
-            else
-            {
-                await this.DisplayAlert("Not found", "Geocoder returns no results", "Close");
-            }
+
             await Task.Delay(1000); // workaround for #30 [Android]Map.Pins.Add doesn't work when page OnAppearing
 
-            var pin = new Pin()
+            var pin = new Pin
             {
-                Type = PinType.Place,
-                Label = "Tokyo SKYTREE",
-                Address = "Sumida-ku, Tokyo, Japan",
-                Position = position,
-                Icon = BitmapDescriptorFactory.FromView(new BindingPinView("A"))
+                Address = address,
+                Position = pinPosition,
+                Icon = BitmapDescriptorFactory.FromView(new BindingPinView("")),
+                Label = $"{name}, {description}, {raiting}"
             };
+
             map.Pins.Add(pin);
-         }
+
+            map.MoveToRegion(MapSpan.FromCenterAndRadius(pinPosition, Distance.FromKilometers(1)));
+            
+        }
+
+        /// <summary>
+        /// Initialize GoogleMaps 
+        /// </summary>
+        private async void InitializeMap()
+        {
+            try
+            {
+                geocoder = new Xamarin.Forms.GoogleMaps.Geocoder();
+                var positions = await geocoder.GetPositionsForAddressAsync("Belarus");
+                if (positions.Any())
+                {
+                    var pos = positions.First();
+                    map.MoveToRegion(MapSpan.FromCenterAndRadius(pos, Distance.FromKilometers(2)));
+                    map.IsShowingUser = true;
+
+                    if (mpVM.MapPins.Any() && isStart)
+                    {
+                        var indicator = new IndicatorLayout();
+                        MainLayout.Children.Add(indicator);
+
+                        int count = mpVM.MapPins.Count();
+                        foreach (var mp in mpVM.MapPins)
+                        {
+                            count--;
+                            Position pinPosition = new Position();
+
+                            var pinPositions = await geocoder.GetPositionsForAddressAsync(mp.Address);
+
+                            if (pinPositions != null && pinPositions.Any())
+                            {
+                                pinPosition = pinPositions.First();
+                            }
+
+                            await Task.Delay(1000); // workaround for #30 [Android]Map.Pins.Add doesn't work when page OnAppearing
+
+                            var pin = new Pin
+                            {
+                                Address = mp.Address,
+                                Position = pinPosition,
+                                Icon = BitmapDescriptorFactory.FromView(new BindingPinView("")),
+                                Label = $"{mp.Name}, {mp.Description}, {mp.Raiting}★ "
+                            };
+
+                            map.Pins.Add(pin);
+
+                            if (count == 0)
+                               map.MoveToRegion(MapSpan.FromCenterAndRadius(pinPosition, Distance.FromKilometers(1)));
+                            
+                        }
+
+                        isStart = false;
+                        MainLayout.Children.Remove(indicator);
+                    }
+                }
+                else
+                {
+                    await this.DisplayAlert("Not found", "Geocoder returns no results", "Close");
+                }
+            }
+            catch (Exception e)
+            {
+                await this.DisplayAlert("Not found", e.Message + " Check your internet", "Close");
+            }
+           
+        }
 
     }
 }
